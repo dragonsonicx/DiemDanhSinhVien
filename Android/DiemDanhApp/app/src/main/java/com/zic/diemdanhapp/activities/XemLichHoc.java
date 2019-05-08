32e65b56b4886a175acc5512c237d75399835fdd
dragonsonicx <minhtri456minh@gmail.com>
2019-05-08 15:56:04 +0700

    Chỉnh lại giao diện XemLichHoc

--- Android/DiemDanhApp/app/src/main/java/com/zic/diemdanhapp/activities/XemLichHoc.java
+++ Android/DiemDanhApp/app/src/main/java/com/zic/diemdanhapp/activities/XemLichHoc.java
@@ -114,14 +114,34 @@ public class XemLichHoc extends AppCompatActivity {
 
     // Tạo text view tương ứng
     private void CreateTextView(String result) {
-        TableRow row2 = findViewById(R.id.row2);
-        TableRow row3 = findViewById(R.id.row3);
-        TableRow row4 = findViewById(R.id.row4);
-        TableRow row5 = findViewById(R.id.row5);
-        TableRow row6 = findViewById(R.id.row6);
-        TableRow row7 = findViewById(R.id.row7);
-        TableRow rowCN = findViewById(R.id.rowCN);
 
+        TableRow row2sang = findViewById(R.id.rowT2Sang);
+        TableRow row2chieu = findViewById(R.id.rowT2Chieu);
+        TableRow row2toi = findViewById(R.id.rowT2Toi);
+
+        TableRow row3sang = findViewById(R.id.rowT3Sang);
+        TableRow row3chieu = findViewById(R.id.rowT3Chieu);
+        TableRow row3toi = findViewById(R.id.rowT3Toi);
+
+        TableRow row4sang = findViewById(R.id.rowT4Sang);
+        TableRow row4chieu = findViewById(R.id.rowT4Chieu);
+        TableRow row4toi = findViewById(R.id.rowT4Toi);
+
+        TableRow row5sang = findViewById(R.id.rowT5Sang);
+        TableRow row5chieu = findViewById(R.id.rowT5Chieu);
+        TableRow row5toi = findViewById(R.id.rowT5Toi);
+
+        TableRow row6sang = findViewById(R.id.rowT6Sang);
+        TableRow row6chieu = findViewById(R.id.rowT6Chieu);
+        TableRow row6toi = findViewById(R.id.rowT6Toi);
+
+        TableRow row7sang = findViewById(R.id.rowT7Sang);
+        TableRow row7chieu = findViewById(R.id.rowT7Chieu);
+        TableRow row7toi = findViewById(R.id.rowT7Toi);
+
+        TableRow row8sang = findViewById(R.id.rowT8Sang);
+        TableRow row8chieu = findViewById(R.id.rowT8Chieu);
+        TableRow row8toi = findViewById(R.id.rowT8Toi);
 
         try {
             JSONArray array = new JSONArray(result);
@@ -133,45 +153,148 @@ public class XemLichHoc extends AppCompatActivity {
                 String giobatdau = obj.getString("gioBatDau");
                 String gioketthuc = obj.getString("gioKetThuc");
                 String tenphonghoc = obj.getString("tenPhongHoc");
+                String buoihoc = obj.getString("buoiHoc");
 
-                switch (thu) {
-                    case "2":
-                        TextView txtView2 = new TextView(this);
-                        CreateTextViewMonHoc(txtView2, row2, tenmonhoc, giobatdau, gioketthuc, tenphonghoc);
-                        break;
-
-                    case "3":
-                        TextView txtView3 = new TextView(this);
-                        CreateTextViewMonHoc(txtView3, row3, tenmonhoc, giobatdau, gioketthuc, tenphonghoc);
-                        break;
-
-                    case "4":
-                        TextView txtView4 = new TextView(this);
-                        CreateTextViewMonHoc(txtView4, row4, tenmonhoc, giobatdau, gioketthuc, tenphonghoc);
-                        break;
-
-                    case "5":
-                        TextView txtView5 = new TextView(this);
-                        CreateTextViewMonHoc(txtView5, row5, tenmonhoc, giobatdau, gioketthuc, tenphonghoc);
-                        break;
-
-                    case "6":
-                        TextView txtView6 = new TextView(this);
-                        CreateTextViewMonHoc(txtView6, row6, tenmonhoc, giobatdau, gioketthuc, tenphonghoc);
-                        break;
-
-                    case "7":
-                        TextView txtView7 = new TextView(this);
-                        CreateTextViewMonHoc(txtView7, row7, tenmonhoc, giobatdau, gioketthuc, tenphonghoc);
-                        break;
-
-                    case "CN":
-                        TextView txtViewCN = new TextView(this);
-                        CreateTextViewMonHoc(txtViewCN, rowCN, tenmonhoc, giobatdau, gioketthuc, tenphonghoc);
-                        break;
-                    default:
-                        break;
+                // Kiểm tra thứ
+                // Thứ 2
+                if (thu.equals("2")) {
+                    //Kiểm tra buổi học
+                    switch (buoihoc) {
+                        case "Sáng":
+                            TextView txtView2sang = new TextView(this);
+                            CreateTextViewMonHoc(txtView2sang, row2sang, tenmonhoc, giobatdau, gioketthuc, tenphonghoc);
+                            break;
+                        case "Chiều":
+                            TextView txtView2chieu = new TextView(this);
+                            CreateTextViewMonHoc(txtView2chieu, row2chieu, tenmonhoc, giobatdau, gioketthuc, tenphonghoc);
+                            break;
+                        case "Tối":
+                            TextView txtView2toi = new TextView(this);
+                            CreateTextViewMonHoc(txtView2toi, row2chieu, tenmonhoc, giobatdau, gioketthuc, tenphonghoc);
+                            break;
+                        default:
+                            break;
+                    }
                 }
+                // Thứ 3
+                else if (thu.equals("3"))
+                    //Kiểm tra buổi học
+                    switch (buoihoc) {
+                        case "Sáng":
+                            TextView txtView3sang = new TextView(this);
+                            CreateTextViewMonHoc(txtView3sang, row3sang, tenmonhoc, giobatdau, gioketthuc, tenphonghoc);
+                            break;
+                        case "Chiều":
+                            TextView txtView3chieu = new TextView(this);
+                            CreateTextViewMonHoc(txtView3chieu, row3chieu, tenmonhoc, giobatdau, gioketthuc, tenphonghoc);
+                            break;
+                        case "Tối":
+                            TextView txtView3toi = new TextView(this);
+                            CreateTextViewMonHoc(txtView3toi, row3chieu, tenmonhoc, giobatdau, gioketthuc, tenphonghoc);
+                            break;
+                        default:
+                            break;
+                    }
+
+                    // Thứ 4
+                else if (thu.equals("4"))
+                    //Kiểm tra buổi học
+                    switch (buoihoc) {
+                        case "Sáng":
+                            TextView txtView4sang = new TextView(this);
+                            CreateTextViewMonHoc(txtView4sang, row4sang, tenmonhoc, giobatdau, gioketthuc, tenphonghoc);
+                            break;
+                        case "Chiều":
+                            TextView txtView4chieu = new TextView(this);
+                            CreateTextViewMonHoc(txtView4chieu, row4chieu, tenmonhoc, giobatdau, gioketthuc, tenphonghoc);
+                            break;
+                        case "Tối":
+                            TextView txtView4toi = new TextView(this);
+                            CreateTextViewMonHoc(txtView4toi, row4chieu, tenmonhoc, giobatdau, gioketthuc, tenphonghoc);
+                            break;
+                        default:
+                            break;
+                    }
+
+                    // Thứ 5
+                else if (thu.equals("5"))
+                    //Kiểm tra buổi học
+                    switch (buoihoc) {
+                        case "Sáng":
+                            TextView txtView5sang = new TextView(this);
+                            CreateTextViewMonHoc(txtView5sang, row5sang, tenmonhoc, giobatdau, gioketthuc, tenphonghoc);
+                            break;
+                        case "Chiều":
+                            TextView txtView5chieu = new TextView(this);
+                            CreateTextViewMonHoc(txtView5chieu, row5chieu, tenmonhoc, giobatdau, gioketthuc, tenphonghoc);
+                            break;
+                        case "Tối":
+                            TextView txtView5toi = new TextView(this);
+                            CreateTextViewMonHoc(txtView5toi, row5chieu, tenmonhoc, giobatdau, gioketthuc, tenphonghoc);
+                            break;
+                        default:
+                            break;
+                    }
+
+                    // Thứ 6
+                else if (thu.equals("6"))
+                    //Kiểm tra buổi học
+                    switch (buoihoc) {
+                        case "Sáng":
+                            TextView txtView6sang = new TextView(this);
+                            CreateTextViewMonHoc(txtView6sang, row6sang, tenmonhoc, giobatdau, gioketthuc, tenphonghoc);
+                            break;
+                        case "Chiều":
+                            TextView txtView6chieu = new TextView(this);
+                            CreateTextViewMonHoc(txtView6chieu, row6chieu, tenmonhoc, giobatdau, gioketthuc, tenphonghoc);
+                            break;
+                        case "Tối":
+                            TextView txtView6toi = new TextView(this);
+                            CreateTextViewMonHoc(txtView6toi, row6chieu, tenmonhoc, giobatdau, gioketthuc, tenphonghoc);
+                            break;
+                        default:
+                            break;
+                    }
+
+                    // Thứ 7
+                else if (thu.equals("7"))
+                    //Kiểm tra buổi học
+                    switch (buoihoc) {
+                        case "Sáng":
+                            TextView txtView7sang = new TextView(this);
+                            CreateTextViewMonHoc(txtView7sang, row7sang, tenmonhoc, giobatdau, gioketthuc, tenphonghoc);
+                            break;
+                        case "Chiều":
+                            TextView txtView7chieu = new TextView(this);
+                            CreateTextViewMonHoc(txtView7chieu, row7chieu, tenmonhoc, giobatdau, gioketthuc, tenphonghoc);
+                            break;
+                        case "Tối":
+                            TextView txtView7toi = new TextView(this);
+                            CreateTextViewMonHoc(txtView7toi, row7chieu, tenmonhoc, giobatdau, gioketthuc, tenphonghoc);
+                            break;
+                        default:
+                            break;
+                    }
+
+                    // Chủ nhật
+                else if (thu.equals("8"))
+                    //Kiểm tra buổi học
+                    switch (buoihoc) {
+                        case "Sáng":
+                            TextView txtView8sang = new TextView(this);
+                            CreateTextViewMonHoc(txtView8sang, row8sang, tenmonhoc, giobatdau, gioketthuc, tenphonghoc);
+                            break;
+                        case "Chiều":
+                            TextView txtView8chieu = new TextView(this);
+                            CreateTextViewMonHoc(txtView8chieu, row8chieu, tenmonhoc, giobatdau, gioketthuc, tenphonghoc);
+                            break;
+                        case "Tối":
+                            TextView txtView8toi = new TextView(this);
+                            CreateTextViewMonHoc(txtView8toi, row8chieu, tenmonhoc, giobatdau, gioketthuc, tenphonghoc);
+                            break;
+                        default:
+                            break;
+                    }
             }
 
         } catch (JSONException e) {
@@ -187,7 +310,7 @@ public class XemLichHoc extends AppCompatActivity {
         t.setTextAlignment(TextView.TEXT_ALIGNMENT_CENTER);
         t.setTextColor(Color.parseColor("#040750"));
         t.setTextSize(18);
-        t.setPadding(2, 2, 2, 2);
+        t.setPadding(5, 5, 5, 5);
         r.addView(t);
     }
 
